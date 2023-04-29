e6178fe9bd907bda3f564b04233ff6cbf0d235c8
Maciej Mejer <m.j.mejer@student.tudelft.nl>
2023-04-29 19:23:50 +0200

    Implement frontend part.

---
+++ script.js
@@ -0,0 +1,74 @@
+let task = "factor";
+
+function doPrimalityTest(e) {
+    if(task == "check-primality") return;
+
+    task = "check-primality";
+    document.querySelector("button").textContent = "check";
+    switchOption(e.currentTarget);
+}
+
+function doFactoring(e) {
+    if(task == "factor") return;
+
+    task = "factor";
+    document.querySelector("button").textContent = "factor";
+    switchOption(e.currentTarget);
+}
+
+/**
+ * switches option to another
+ * @param other - the option we are switching to
+ */
+function switchOption(other) {
+    let current = document.querySelector(".optionActive");
+    // turn off current option
+    if(current)
+    {
+        current.classList.remove("optionActive");
+        current.classList.add("optionInactive");
+    }
+
+    // turn on other option
+    other.classList.remove("optionInactive");
+    other.classList.add("optionActive");
+
+    // clear result
+    document.querySelector("#result").textContent = "---";
+}
+
+/**
+ * input submision
+ */
+document.querySelector("form").addEventListener("submit", function(e) {
+    let data = document.querySelector("#inputNumber").value;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "http://localhost:5000/" + task, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        let output;
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            output = xhr.responseText;
+            if(task == "factor") {
+                output = output.slice(0, -1);
+                output = output.replaceAll("\n", " \u00B7 ");
+            }
+        }
+        else {
+            output = "server error";
+        }
+        document.querySelector("#result").textContent = output;
+    };
+    xhr.send(data);
+
+    e.preventDefault();
+});
+
+function isDigit(e) {
+    e = (e) ? e : window.event;
+    var charCode = (e.which) ? e.which : e.keyCode;
+    if (charCode < 31 || (charCode >= 48 && charCode <= 57)) return true;
+    return false;
+}
--- server.py
+++ server.py
@@ -1,27 +1,33 @@
 import subprocess
-
 from flask import Flask, request
+from flask_cors import CORS
+from flask_cors import cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route("/factor", methods=['POST'])
+@cross_origin()
 def factor():
     data = request.get_data(as_text=True)
+    print("factor" + data + "\n")
 
     output = subprocess.check_output(['Algorithms/cmake-build-debug/bin/Integer_factorization-pollards_rho'], input=data,
                                      text=True)
-    # print(output)
+    print("out: " + output)
     return output
 
 
 @app.route("/check-primality", methods=['POST'])
+@cross_origin()
 def check_primality():
     data = request.get_data(as_text=True)
+    print("test" + data + "\n")
 
     output = subprocess.check_output(['Algorithms/cmake-build-debug/bin/Primality_test-miller_rabin'], input=data,
                                      text=True)
-    # print(output)
+    print("out: " + output)
     return output
 
 
